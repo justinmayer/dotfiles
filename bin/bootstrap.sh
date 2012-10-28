0f8240f6535ff502ce29da3128a267954e91b8a9
Justin Mayer <entroP@gmail.com>
2012-10-28 13:42:29 -0700

    Initial bootstrap script, mostly Mac-specific. Initial barebones vimrc.

---
+++ bin/bootstrap.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+export ARCHFLAGS="-arch x86_64"
+export OS=`uname -s | sed -e 's/  */-/g;y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/'`
+
+if [ "$OS" = "darwin" ]; then
+    TOOLS_HOME=$HOME/Tools
+    PROJECTS_HOME=$HOME/Projects
+    VIRTUALENVS_HOME=$HOME/Virtualenvs
+    PYHOME=/usr/local
+
+    # Install Homebrew
+    if [ ! -f /usr/local/bin/brew ]; then
+        /usr/bin/ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+    fi
+
+    # Install Python with up-to-date OpenSSL
+    if [ ! -f /usr/local/bin/python ]; then
+        brew install python --with-brewed-openssl
+        PATH=/usr/local/share/python:/usr/local/bin:$PATH
+        $PYHOME/bin/pip install --upgrade distribute
+    fi
+
+    # Install Mercurial, hg-git, and Virtualenv:
+    $PYHOME/bin/pip install Mercurial hg-git virtualenv
+
+    # If Dropbox is present, symlink the Projects contained within
+    if [ -d ~/Dropbox/Projects/dotfiles ]; then
+        test -L "$PROJECTS_HOME" || ln -s "$HOME/Dropbox/Projects" "$PROJECTS_HOME"
+        test -L "$TOOLS_HOME" || ln -s "$HOME/Dropbox/Projects/dotfiles" "$TOOLS_HOME"
+    fi
+fi
+
+mkdir -p $VIRTUALENVS_HOME
+
+# Create .hgrc and .hgrc_local files if not present
+if [ ! -f ~/.hgrc ]; then
+    echo -e "\nNo ~/.hgrc detected."
+    echo -e "\n[hostfingerprints]\nbitbucket.org = 24:9c:45:8b:9c:aa:ba:55:4e:01:6d:58:ff:e4:28:7d:2a:14:ae:3b" > ~/.hgrc
+    echo -e "\n# Local settings\n%include ~/.hgrc_local" >> ~/.hgrc
+    echo -e "\n" && read -p "Enter your full name: " -e FULLNAME
+    read -p "Enter your email address: " -e EMAIL
+    echo -e "[ui]\nusername = $FULLNAME <$EMAIL>" > ~/.hgrc_local
+fi
+
+# Retrieve command-line tools (if Dropbox is not present)
+test -d $TOOLS_HOME || hg clone https://bitbucket.org/j/dotfiles $TOOLS_HOME
+
+# Ensure symlinks
+function ensure_link {
+    test -L "$HOME/$2" || ln -s "$TOOLS_HOME/$1" "$HOME/$2"
+}
+
+ensure_link "hgignore"                         ".hgignore"
+ensure_link "vim"                              ".vim"
+ensure_link "vim/vimrc"                        ".vimrc"
