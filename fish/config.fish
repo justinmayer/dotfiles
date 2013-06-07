862eda197fbbbfeccaf7249f5a18cb0a22276890
Justin Mayer <entroP@gmail.com>
2013-06-06 18:47:08 -0700

    Fish: fix environment variable scoping

--- fish/config.fish
+++ fish/config.fish
@@ -10,13 +10,12 @@ prepend_to_path "/usr/local/share/python"          # Mac pip-installed binaries
 prepend_to_path "$HOME/.local/bin"
 deduplicate PATH                                   # Clear duplicates in path
 
+set fish_greeting
 
-
-set -g -x fish_greeting
-set -g -x ARCHFLAGS "-arch x86_64"
-set -g -x EDITOR vim
+set -x ARCHFLAGS "-arch x86_64"
+set -x EDITOR vim
 
 # Python variables
 
-set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
-set -g -x PIP_REQUIRE_VIRTUALENV "true"
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
+set -x PIP_REQUIRE_VIRTUALENV "true"
