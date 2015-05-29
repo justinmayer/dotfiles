2806e7bbe3ac6015b2721f1ae32d0f14a556e4ef
Justin Mayer <entroP@gmail.com>
2015-05-29 15:39:19 -0700

    Pip now uses own cache; former setting deprecated

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,6 @@ set -x PIP_REQUIRE_VIRTUALENV "true"
 set -x PIP_USE_WHEEL "true"
 set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
 set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
-set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 set -x PIP_TIMEOUT 15
 set -x PIP_ALLOW_ALL_EXTERNAL "false"
 set -x PIP_NO_ALLOW_INSECURE "false"
