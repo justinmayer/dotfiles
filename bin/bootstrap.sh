30aabdb198799fc2550372b9481720bb8e57dd5c
Justin Mayer <entroP@gmail.com>
2015-06-30 11:23:06 -0700

    Switch to Powerline releases instead of master
    Releases weren't available previously. Now that they are, we should use them.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -95,7 +95,7 @@ unset PIP_REQUIRE_VIRTUALENV
 [ ! -f /usr/local/bin/hg ] && $PIP_PREFIX install Mercurial
 [ ! -f /usr/local/bin/dulwich ] && $PIP_PREFIX install hg-git
 [ ! -f /usr/local/bin/isort ] && $PIP_PREFIX install isort
-[ ! -f /usr/local/bin/powerline ] && $PIP_PREFIX install git+git://github.com/Lokaltog/powerline
+[ ! -f /usr/local/bin/powerline ] && $PIP_PREFIX install powerline-status
 export PIP_REQUIRE_VIRTUALENV="true"
 
 # If .hgrc or .gitlocal isn't present, ask for name and email address
