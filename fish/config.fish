bb26ca6c73aba156d3236b291e80867e86720189
Justin Mayer <entroP@gmail.com>
2015-08-11 17:24:38 -0700

    Add iTerm shell integration for Fish

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,7 @@ set tacklebox_modules virtualfish virtualhooks
 set tacklebox_plugins docker extract hg grc pip python up vundle
 set tacklebox_theme entropy
 source $HOME/.tacklebox/tacklebox.fish
+source $HOME/.config/fish/iterm.fish
 
 # Set PATH --------------------------------------------------------------------
 
