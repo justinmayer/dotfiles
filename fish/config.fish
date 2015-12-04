836c4a3b005690f194c574261e5ad6787ba27f47
Justin Mayer <entroP@gmail.com>
2015-12-04 11:37:43 -0800

    Fix `~/projects` capitalization to support Linux

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,7 @@ set -x PIP_ALLOW_ALL_EXTERNAL "false"
 set -x PIP_NO_ALLOW_INSECURE "false"
 
 set -x WORKON_HOME "$HOME/virtualenvs"
-set -x PROJECT_HOME "$HOME/Projects"
+set -x PROJECT_HOME "$HOME/projects"
 set -g VIRTUALFISH_HOME $WORKON_HOME
 set -g VIRTUALFISH_COMPAT_ALIASES "True"
 
