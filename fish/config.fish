d1461bad57b2abfda8186fcca9abb4436e06df1d
Justin Mayer <entroP@gmail.com>
2017-06-09 12:05:09 -0700

    Replace Pip env vars with Pip configuration file
    Many of these environment variables were obsolete and thus removed outright.
    The others were moved to a Pip configuration file, which currently is moved
    to its proper location on macOS/Linux via Ansible instead of the increasingly
    moot bootstrap.sh file. Latter will eventually go away in favor of Ansible-
    powered bootstrapping.

--- fish/config.fish
+++ fish/config.fish
@@ -8,15 +8,6 @@ set -x LC_ALL en_US.UTF-8
 
 # Python variables
 
-set -x PIP_REQUIRE_VIRTUALENV "true"
-set -x PIP_USE_WHEEL "true"
-set -x PIP_CACHE_DIR "$HOME/.cache/pip"
-set -x PIP_WHEEL_DIR "$PIP_CACHE_DIR/wheels"
-set -x PIP_FIND_LINKS "$PIP_WHEEL_DIR"
-set -x PIP_TIMEOUT 15
-set -x PIP_ALLOW_ALL_EXTERNAL "false"
-set -x PIP_NO_ALLOW_INSECURE "false"
-
 set -x WORKON_HOME "$HOME/virtualenvs"
 set -x PROJECT_HOME "$HOME/projects"
 set -g VIRTUALFISH_HOME $WORKON_HOME
