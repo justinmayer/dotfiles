4a7bd0a9c2b92b9d24e0cc01f68a21d1d2fb4267
Justin Mayer <entroP@gmail.com>
2026-02-19 17:45:44 +0100

    Pacify code linter for pythonrc.py

--- dot_pythonrc.py
+++ dot_pythonrc.py
@@ -1,5 +1,5 @@
 try:
-    from rich import print, pretty, inspect, traceback
+    from rich import print, pretty, inspect, traceback  # noqa: F401
     pretty.install()
     _ = traceback.install(show_locals=True)
     print("[bold green]Rich enabled :tada:")
